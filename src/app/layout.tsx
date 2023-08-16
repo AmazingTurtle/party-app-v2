c0b81ae0092bc10f25cf5146308a69a69375b27b
Felix Faust <felix@turtledev.net>
2023-08-16 14:13:48 +0200

    icon + metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,42 @@ export const metadata: Metadata = {
   title: 'The Party App',
   description: 'Eine Party App für die Party People',
   manifest: '/site.webmanifest',
+  icons: ['/logo-1024x1024.png'],
   themeColor: '#0A1012',
   appleWebApp: {
     title: 'Party App',
     capable: true,
     statusBarStyle: 'black-translucent',
     startupImage: '/logo-1024x1024.png',
+  },
+  alternates: {
+    canonical: 'https://new.thepartyapp.xyz/',
+  },
+  openGraph: {
+    title: 'Party App',
+    description: 'Eine Party App für die Party People',
+    type: 'website',
+    url: 'https://new.thepartyapp.xyz/',
+    images: [
+      {
+        type: 'image/png',
+        width: 1024,
+        height: 1024,
+        url: 'https://new.thepartyapp.xyz/logo-1024x1024.png',
+      },
+      {
+        type: 'image/png',
+        width: 512,
+        height: 512,
+        url: 'https://new.thepartyapp.xyz/logo-512x512.png',
+      },
+      {
+        type: 'image/png',
+        width: 192,
+        height: 192,
+        url: 'https://new.thepartyapp.xyz/logo-192x192.png',
+      },
+    ],
   },
 };
 
